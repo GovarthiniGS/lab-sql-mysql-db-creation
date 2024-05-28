fa64d42fe5fe865ef61d08369b9bf3b29583e9a3
Govarthinigs <govarthini.gopalan@gmail.com>
2024-05-28 10:25:11 +0200

    seeding.sql done

---
+++ update.sql
@@ -0,0 +1,12 @@
+
+USE CarSalesDB;
+SET SQL_SAFE_UPDATES = 0;
+UPDATE Customers SET Email = 'ppicasso@gmail.com' WHERE Name = 'Pablo Picasso';
+UPDATE Customers SET Email = 'lincoln@us.gov' WHERE Name = 'Abraham Lincoln';
+UPDATE Customers SET Email = 'hello@napoleon.me' WHERE Name = 'Napoléon Bonaparte';
+SET SQL_SAFE_UPDATES = 1;
+
+USE CarSalesDB;
+SET SQL_SAFE_UPDATES = 0;
+DELETE FROM Cars WHERE CarID = 4;
+SET SQL_SAFE_UPDATES = 1;
